b4a4cd3bbe95ca765931427bfb2b91c124e89920
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-12 10:44:50 +0100

    Add project files.

---
+++ C-sharp-Tutorial - Building a Guessing Game/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C_sharp_Tutorial___Building_a_Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+
+            while (guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+             
+                guessCount++;
+
+                if (guessCount > 2)
+                {
+                    Console.Write("Out of guesses");
+                    return;
+                }
+            }
+            Console.Write("You Win!");
+            Console.ReadLine();
+
+            //do
+
+            //{
+            //    Console.Write("Enter guess: ");
+            //    guess = Console.ReadLine();
+            //}
+
+            //while (guess != secretWord);
+            //Console.Write("You Win!");
+        }
+
+
+    }
+}
